34dd340d8512e021d84b9d4ea402799500f62f3c
iabsensimaster <iabsensimaster@gmail.com>
2025-05-03 12:30:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_ENDPOINT = "https://script.google.com/macros/s/AKfycby4FsJAMXtGMYEzOwlrmjFpPMx73mZLKaVFooooOPpv2LoyierjuB23CyVvmgWK7Ni1/exec";
+const API_ENDPOINT = "https://script.google.com/macros/s/AKfycbwXfA_x8_UNxdhAaB2ljbNhImK0jhllmcCFZ29phcPv2PFzLewUF7dI7y2Suw4PkKZz/exec";
